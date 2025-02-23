5ed6bc11d453698e7a7749a93afddc38529adae1
fayed <fayed_rana@outlook.com>
2025-02-23 15:33:13 +0100

    chapter_2 solutions added

--- bootdev/chapter_2/Posts_table_migration/main.sql
+++ bootdev/chapter_2/Posts_table_migration/main.sql
@@ -1,5 +1,19 @@
+/*
 
+ALTER TABLE posts
+  RENAME COLUMN author_id to poster_id;
+
+ALTER TABLE posts
+  ADD COLUMN  is_edited BOOLEAN;
+
+ALTER TABLE posts
+  DROP COLUMN is_sponsored;
+
+
+*/
 
 -- TEST SUITE, DON'T TOUCH BELOW THIS LINE --
 
-PRAGMA TABLE_INFO('posts');
+
+-- REMOVE COMMENT BELOW --
+-- PRAGMA TABLE_INFO('posts'); -- 
